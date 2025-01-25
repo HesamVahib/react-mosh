3f81a671fc9d960d3199ffb080504ac3c525f489
Hesam V. <hesam.vahib@gmail.com>
2025-01-25 02:38:13 +0200

    definition in TS

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,15 @@
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from 'react';
 import ListGroup from "./components/ListGroup";
 
 function App() {
+
+  const items = ["Item 1", "Item 2", "Item 3"];
+
   return (
     <>
-      <ListGroup />
+      <ListGroup items={items} heading="Select an item" onSelectItem={(item) => console.log(item)} />
     </>
   );
 }
