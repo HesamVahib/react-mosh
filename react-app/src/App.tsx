c9b75054e5b9785b9f06a69476475888efb60020
Hesam V. <hesam.vahib@gmail.com>
2025-02-05 01:44:44 +0200

    using useForm

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <ExpandableText maxLength={100}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod obcaecati odit non quidem eius ducimus, itaque ea! Mollitia dolor eaque a provident facere? Non itaque eum fugiat, eaque sint accusantium iusto alias quas ullam excepturi nobis ea dolor possimus doloribus blanditiis. Blanditiis corrupti incidunt corporis quasi ipsum voluptates voluptate sint eligendi veniam eos, totam, amet quis eveniet cupiditate? Rerum dolores reprehenderit exercitationem ex delectus expedita in, ad quod. Voluptatum voluptate consectetur tenetur delectus magni sed non doloremque quos animi! Eos quidem dolor veniam impedit, consectetur reiciendis aut nihil voluptatem possimus quos, error eius rerum excepturi unde mollitia aperiam saepe cupiditate.
+        Hello World
       </ExpandableText>
     </>
   );
--- react-app/src/components/ExpandableText.tsx
+++ react-app/src/components/ExpandableText.tsx
@@ -1,5 +1,4 @@
-import React, {useState} from 'react'
-
+import {useState} from 'react';
 
 interface ExpandableTextProps {
     children: string;
@@ -7,15 +6,18 @@ interface ExpandableTextProps {
 }
 
 const ExpandableText = ({children, maxLength}: ExpandableTextProps) => {
+    if (children.length <= maxLength) return <div>{children}</div>;
+
     const [isExpanded, setIsExpanded] = useState(false);
 
-    const expand = () => {
-        setIsExpanded(!isExpanded);
-    }
+    const expand = () => setIsExpanded(!isExpanded);
 
-    const text = isExpanded ? children : children.substring(0, maxLength);
+    const text = isExpanded ?
+        children : children.substring(0, maxLength);
+
   return (
-    <div>{text}{!isExpanded ? '...' : null} <button onClick={expand}>{!isExpanded ? 'More' : 'Less'}</button></div>
+    <div>{text}{!isExpanded ? '...' : null} <button onClick={expand}>{!isExpanded ? 'More' : 'Less'}</button>
+    </div>
   )
 }
 
